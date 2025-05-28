ecce14c5017128dac3dabf82f1473425c230da0a
Loy <blortega3@gmail.com>
2025-05-28 14:08:19 +0800

    feat: - Added Forgot-Password with accompanying CSS - Implemented route for Forgot-Password

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import ForgotPassword from "./pages/Forgot-Password";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
     </Routes>
   );
 }
